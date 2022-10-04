0b56e4abc1941a151bae22bd54aee7d97c29133a
Jeremy Martin <48764226+jer-martin@users.noreply.github.com>
2022-10-04 13:15:58 -0400

    parsing
    fixed a few parsing issues, should clean data up significantly better now

--- terpcleaner.py
+++ terpcleaner.py
@@ -1,10 +1,15 @@
 import fileinput
-outfile = open("outterp.txt", "w")
+outfile = open("outterp.txt", "w", encoding="utf-8")
 
 
 
-for line in fileinput.input("outpdf.txt"):
+for line in fileinput.input("outpdf.txt", encoding="utf-8"):
+    if "Trulieve" in line or "Total" in line or "Plants" in line or "plant" in line or "Matrix" in line:
+        continue
     if "Tru" in line or "Flower" in line or "Flowr" in line or "Ground" in line:
+        outfile.write('\n')
         outfile.write(line)
-    if "0." in line and "%" in line:
+        outfile.write('\n')
+    if "%" in line and "." in line:
         outfile.write(line)
+
