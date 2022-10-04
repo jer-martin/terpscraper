91b766c1942adeaf3b08fa91ead474832b8df3cf
Jeremy Martin <48764226+jer-martin@users.noreply.github.com>
2022-10-04 13:30:57 -0400

    added runTS
    this runs all of the scripts needed

---
+++ runTS.py
@@ -0,0 +1,8 @@
+import os
+
+
+os.system('python3 terpinput.py')
+os.system('python3 terpscraper.py')
+os.system('python3 terpdownloader.py')
+os.system('python3 terppdf.py')
+os.system('python3 terpcleaner.py')
