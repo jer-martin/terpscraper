af1c64fc286c31f0b335b2cb1abf4024c06a0120
Jeremy Martin <48764226+jer-martin@users.noreply.github.com>
2022-10-04 13:19:40 -0400

    now deletes extra files

--- terpdownloader.py
+++ terpdownloader.py
@@ -1,19 +1,26 @@
-import PyPDF2
-import urllib
-import urllib.request
-import requests
-from pathlib import Path
 import fileinput
-from PyPDF2 import PdfMerger
+import glob
+import os
 
-count = 0
-for line in fileinput.input(files = "../coalist.txt"):
-    url = line.strip()
-    out = 'terps'
-    response = requests.get(url)
-    pdf = open(out+str(count)+'.pdf', 'wb')
-    pdf.write(response.content)
-    count = count + 1
+pdflist = glob.glob("*.pdf")
+outfile = open("outterp.txt", "w", encoding="utf-8")
 
 
-pdf.close()
+
+for line in fileinput.input("outpdf.txt", encoding="utf-8"):
+    if "Trulieve" in line or "Total" in line or "Plants" in line or "plant" in line or "Matrix" in line:
+        continue
+    if "Tru" in line or "Flower" in line or "Flowr" in line or "Ground" in line:
+        outfile.write('\n')
+        outfile.write(line)
+        outfile.write('\n')
+    if "%" in line and "." in line:
+        outfile.write(line)
+
+
+for file in pdflist:
+    os.remove(file)
+
+os.remove('coalist.txt')
+os.remove('outlinks.txt')
+os.remove('outpdf.txt')
